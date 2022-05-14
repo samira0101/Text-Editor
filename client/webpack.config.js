f74512004fbda4362ca026649a19ac57f6b8bb7f
Allure <samira-h@hotmail.co.uk>
2022-05-14 23:20:56 +0100

    Output for our bundles

---
+++ client/webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    // Entry point for files
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    // Output for our bundles
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
