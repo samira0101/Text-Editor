293a946045c50da5338e30fb5a4c01e8da3b7d0e
Allure <samira-h@hotmail.co.uk>
2022-05-14 23:25:25 +0100

    Import methods to save and get data from  database

---
+++ client/src/js/editor.js
@@ -0,0 +1,23 @@
+// Import methods to save and get data from the indexedDB database in './database.js'
+import { getDb, putDb } from './database';
+import { header } from './header';
+
+export default class {
+  constructor() {
+    const localData = localStorage.getItem('content');
+
+    // check if CodeMirror is loaded
+    if (typeof CodeMirror === 'undefined') {
+      throw new Error('CodeMirror is not loaded');
+    }
+
+    this.editor = CodeMirror(document.querySelector('#main'), {
+      value: '',
+      mode: 'javascript',
+      theme: 'monokai',
+      lineNumbers: true,
+      lineWrapping: true,
+      autofocus: true,
+      indentUnit: 2,
+      tabSize: 2,
+    });
