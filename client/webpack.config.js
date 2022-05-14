d8bbb4802a861385f40ad837230bdf4ff31ddcf2
Allure <samira-h@hotmail.co.uk>
2022-05-14 23:22:15 +0100

    plugin generates  html file and injects bundles.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,3 +17,35 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+            // Webpack plugin that generates our html file and injects our bundles. 
+           new HtmlWebpackPlugin({
+            template: './index.html',
+            title: 'JATE'
+          }),
+          
+          // // Injects our custom service worker
+          new InjectManifest({
+            swSrc: './src-sw.js',
+            swDest: 'src-sw.js',
+          }),
+          // Creates a manifest.json
+          new WebpackPwaManifest({
+            fingerprints: false,
+            inject: true,
+            name: 'text-editor',
+            short_name: 'JATE',
+            description: 'Just Another Text Editor!',
+            background_color: '#225ca3',
+            theme_color: '#225ca3',
+            start_url: '/',
+            publicPath: '/',
+            icons: [
+              {
+                src: path.resolve('src/images/logo.png'),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join('assets', 'icons'),
+              },
+            ],
+          }), 
+    ],
